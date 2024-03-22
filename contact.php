e7eac1c65d414a475664c6fe9d54535ecd5e77ce
silviocn1 <112991253+silviocn1@users.noreply.github.com>
2024-03-22 16:57:55 -0700

    added functionality to change username
    MADE about us page

--- contact.php
+++ contact.php
@@ -42,6 +42,7 @@
             </div>
         </div>
     </div>
+<<<<<<< Updated upstream
 
     <footer class="bg-dark text-light py-4">
         <div class="container">
@@ -59,6 +60,11 @@
             </div>
         </div>
     </footer>
+=======
+    <?php
+        include("footer.php");
+    ?>
+>>>>>>> Stashed changes
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>

adfdea7c57cd9352bdf5d5237ba10f7de7da9a2f
silviocn1 <112991253+silviocn1@users.noreply.github.com>
2024-03-23 16:28:03 -0700

    Updated about us page
    Put website and creator information in the file

--- about_us.php
+++ about_us.php
@@ -18,20 +18,21 @@
 <body>
     <div class="container mt-4">
         <h1>About Us</h1>
-        <p class="lead">[Your website description here]</p>
+        <p class="lead">Insight Globe is a website created from the inspiration of the creators to bring together people worldwide to share ideas about diverse subjects. Founded on the principle that knowledge empowers individuals, we strive to deliver content that enlightens, informs, and inspires. We seek to make people as connected as possible and to be able to engage in friendly discussions. </p>
 
         <div class="row">
             <div class="col-md-4">
-                <h2>Creator 1</h2>
+                <h2>Aditya Tripathy</h2>
                 <p>[Description of creator 1]</p>
             </div>
             <div class="col-md-4">
-                <h2>Creator 2</h2>
+                <h2>João Bolsonaro</h2>
                 <p>[Description of creator 2]</p>
             </div>
             <div class="col-md-4">
-                <h2>Creator 3</h2>
-                <p>[Description of creator 3]</p>
+                <h2>Silvio Carvalho</h2>
+                <p>Economics major fourth year-student with experience in financial planning and analysis, applying excel to financial models and financial forecasting, and technical skills in Java, Python, SQL, and R studio. Undertook this website as a project and for applying CSS, PHP, and HTML skills.
+</p>
             </div>
         </div>
     </div>

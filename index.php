2d17523638f41c2ddde8c5bc2ca6712f2ed46928
jhlbolsonaro <113214459+jhlbolsonaro@users.noreply.github.com>
2024-03-22 18:28:24 -0700

    Updated dashboard, index and created view_posts_index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
                             <h3><?php echo htmlspecialchars($row['title']); ?></h3>
                             <p><?php echo nl2br(htmlspecialchars(substr($row['content'], 0, 100))); ?>...</p>
                             <p class="text-muted">Posted by <?php echo htmlspecialchars($row['user']); ?></p>
-                            <a href="view_post_index.php?id=<?php echo $row['id']; ?>" class="stretched-link">Read More</a>
+                            <a href="view_post.php?id=<?php echo $row['id']; ?>" class="stretched-link">Read More</a>
                         </div>
                     </div>
                     <?php endwhile; ?>
@@ -60,23 +60,32 @@
                 <div class="categories-area">
                     <h3>POSTS BY CATEGORY:</h3>
                     <br>
-                    <button>Health & Wellness</button>
-                    <button>Fitness & Exercise</button>
-                    <button>Personal Development</button>
-                    <button>Finance & Investing</button>
-                    <button>Travel & Destinations</button>
-                    <button>Video Games & Gaming Culture</button>
-                    <button>Photography & Videography</button>
-                    <button>Music & Concerts</button>
-                    <button>Art & Design</button>
-                    <button>Technology & Gadgets</button>
+                    <a href="view_posts_index.php?category=<?php echo urlencode('Health & Wellness'); ?>" class="btn btn-primary">Health & Wellness</a>
+                    <br>
+                    <a href="view_posts_index.php?category=<?php echo urlencode('Fitness & Exercise'); ?>" class="btn btn-primary">Fitness & Exercise</a>
+                    <br>
+                    <a href="view_posts_index.php?category=<?php echo urlencode('Finance & Investing'); ?>" class="btn btn-primary">Finance & Investing</a>
+                    <br>
+                    <a href="view_posts_index.php?category=Personal Development" class="btn btn-primary">Personal Development</a>
+                    <br>
+                    <a href="view_posts_index.php?category=<?php echo urlencode('Travel & Destinations'); ?>" class="btn btn-primary">Travel & Destinations</a>
+                    <br>
+                    <a href="view_posts_index.php?category=<?php echo urlencode('Video Games & Gaming Culture'); ?>" class="btn btn-primary">Video Games & Gaming Culture</a>
+                    <br>
+                    <a href="view_posts_index.php?category=<?php echo urlencode('Photography & Videography'); ?>" class="btn btn-primary">Photography & Videography</a>
+                    <br>
+                    <a href="view_posts_index.php?category=<?php echo urlencode('Music & Concerts'); ?>" class="btn btn-primary">Music & Concerts</a>
+                    <br>
+                    <a href="view_posts_index.php?category=<?php echo urlencode('Art & Design'); ?>" class="btn btn-primary">Art & Design</a>
+                    <br>
+                    <a href="view_posts_index.php?category=<?php echo urlencode('Technology & Gadgets'); ?>" class="btn btn-primary">Technology & Gadgets</a>
                 </div>
 
                 <div class="older-posts-area">
                     <h3>OLDER POSTS:</h3>
                     <br>
                     <?php
-                        $older_posts_query = "SELECT * FROM posts WHERE id ORDER BY created_at DESC ";
+                        $older_posts_query = "SELECT * FROM posts WHERE id ORDER BY created_at DESC";
                         $older_posts_stmt = $conn->prepare($older_posts_query);
                         $older_posts_stmt->execute();
                         $older_posts_result = $older_posts_stmt->get_result();
@@ -84,8 +93,9 @@
                             while ($post = $older_posts_result->fetch_assoc()):
                     ?>
                     <div class="older-post">
+                        <h3 class="post-title"><?php echo htmlspecialchars($post['title']); ?></h3>
                         <p><?php echo htmlspecialchars(substr($post['content'], 0, 100)); ?>...</p>
-                        <button>READ MORE</button>
+                        <a href="view_post.php?id=<?php echo $post['id']; ?>" class="read-more-older">READ MORE</a>
                         <p class="posted-by">POSTED BY: <?php echo htmlspecialchars($post['user']); ?></p>
                     </div>
                     <?php
@@ -96,12 +106,12 @@
             </div>
         </main>
         <?php
-            include("footer_index.php");
+            include("footer.php");
         ?>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <link rel="stylesheet" href="CSS/index_header_style.css">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        </div>
+        <link rel="stylesheet" href="CSS/index_header_style.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>

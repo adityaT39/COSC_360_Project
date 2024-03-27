e0026ddeec5e2ce01da57de66ff887b8ef6d92f7
aditya39p <adityavsec@gmail.com>
2024-03-27 15:32:40 -0700

    Update database.php

--- database.php
+++ database.php
@@ -5,7 +5,7 @@
     $db_pass = "65268641";
     $db_name = "db_65268641";
     $conn = "";
-    $pdo = new PDO('mysql:host=localhost;dbname=db_65268641', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=db_65268641', '65268641', '65268641');
 
     try {
         $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);

6903e7a8b94eb5565a94fc052d8237c3f1d1434b
aditya39p <adityavsec@gmail.com>
2024-03-27 14:44:09 -0700

    Update database.php

--- database.php
+++ database.php
@@ -3,9 +3,9 @@
     $db_server = "localhost";
     $db_user = "root";
     $db_pass = "";
-    $db_name = "db_82056300";
+    $db_name = "db_65268641";
     $conn = "";
-    $pdo = new PDO('mysql:host=localhost;dbname=cosc_360_db', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=db_65268641', 'root', '');
 
     try {
         $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);

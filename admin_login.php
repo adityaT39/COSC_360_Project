eb42aaaecdc8328600b9f8fb81e2ce63443c612a
adityaT39 <adityavsec@gmail.com>
2024-04-10 11:25:10 -0700

    changed the about us page

--- admin_login.php
+++ admin_login.php
@@ -7,7 +7,6 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        // Example of checking credentials (Ensure to use password hashing in a real application)
         $query = "SELECT * FROM admin WHERE username = ?";
         $stmt = $conn->prepare($query);
         $stmt->bind_param("s", $username);
@@ -16,16 +15,16 @@
         if ($result->num_rows > 0) {
             $admin = $result->fetch_assoc();
             if ($password == $admin['password']) {
-                // Correct credentials
+               
                 $_SESSION['admin_logged_in'] = true;
                 header("Location: admin_index.php");
                 exit();
             } else {
-                // Wrong password
+               
                 $login_error = "Invalid username or password. Debug: Entered password is {$password}, DB password is {$admin['password']}";
             }
         } else {
-            // No user found
+            
             $login_error = "Invalid username or password.";
         }
     }
--- contact.php
+++ contact.php
@@ -50,10 +50,6 @@
     <?php
         include("footer.php");
     ?>
-<<<<<<< Updated upstream
-=======
-    <link rel="stylesheet" href="CSS/index_header_style.css">
->>>>>>> Stashed changes
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
